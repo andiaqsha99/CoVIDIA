2e4e8b46e3f2c0edcc72ca9b6550204589539e6f
andiaqsha99 <andi77aqsha@gmail.com>
2020-04-19 19:33:23 +0700

    membuat tampilan covid dan dengue

---
+++ app/src/main/java/com/komas/www/covidia/CovidActivity.kt
@@ -0,0 +1,12 @@
+package com.komas.www.covidia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CovidActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid)
+    }
+}
