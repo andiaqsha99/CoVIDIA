4b0d3c403fb6c0682b473e3f42e35d65329a1ea6
andiaqsha99 <andi77aqsha@gmail.com>
2020-04-26 22:29:26 +0700

    add fitur call for covid

--- app/src/main/java/com/komas/www/covidia/covid/CallCovidActivity.kt
+++ app/src/main/java/com/komas/www/covidia/covid/CallCovidActivity.kt
@@ -1,8 +1,11 @@
 package com.komas.www.covidia.covid
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.komas.www.covidia.R
+import kotlinx.android.synthetic.main.activity_call_covid.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class CallCovidActivity : AppCompatActivity() {
@@ -13,6 +16,11 @@ class CallCovidActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         tv_title_toolbar.text = "Call Urgent"
+
+        btn_call_covid.setOnClickListener {
+            val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:119"))
+            startActivity(dialPhoneIntent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
